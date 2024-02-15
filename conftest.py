5f1ee0f7a63c027a2f4da07afe3eaa8f189e44b9
Andrey Rusak <dudzy@mail.ru>
2024-02-16 01:03:30 +0300

    Добавил conftest.py без доп материалов к отчету и запуска через selenoid

--- conftest.py
+++ conftest.py
@@ -1,11 +1,11 @@
-import os
+# import os
 
 import pytest
 from selene import browser
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from utils import attach
-from dotenv import load_dotenv
+# from selenium import webdriver
+# from selenium.webdriver.chrome.options import Options
+# from utils import attach
+# from dotenv import load_dotenv
 
 
 # @pytest.fixture(scope='session', autouse=True)
@@ -33,10 +33,10 @@ def setup_browser():
     # )
 
     # browser.config.driver = driver
-    browser.config.base_url = "https://nuum.ru"
+    browser.config.base_url = "https://amwine.ru"
     browser.config.window_width = '1920'
     browser.config.window_height = '1080'
-    browser.config.timeout = 2.0
+    browser.config.timeout = 6.0
 
     yield browser
 
