443722bbddf22aa0504cfd22286ec27696fd5d15
Andrey Rusak <dudzy@mail.ru>
2024-02-25 17:26:43 +0300

    Удалил api_functions.py

--- api/requests/api_functions.py
+++
@@ -1,6 +0,0 @@
-from .cart_api import CartApi
-from data.products import Product
-import allure
-# def get_profile():
-#     allure.step()
-#     return CartApi().add_to_cart_via_api(product: Product, quantity)
